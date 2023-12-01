a1e4068230db88733296dd9c89ad52b1a464e0d3
Joshua Herrmann <jdvh1292@gmail.com>
2023-11-30 20:42:49 -0500

    Setup

---
+++ day1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func part1() {}
+
+func part2() {}
+
+func main() {
+	p1b := time.Now()
+	part1()
+	mid := time.Now()
+	part2()
+	p2a := time.Now()
+	part1Time := mid.Sub(p1b)
+	part2Time := p2a.Sub(mid)
+	fmt.Printf("Part 1 Time: %dμs\nPart 2 Time: %dμs\n", part1Time.Microseconds(), part2Time.Microseconds())
+}
