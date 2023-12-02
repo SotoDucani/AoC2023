b2f1620e362bbb2fd37d13de93c67ae11cda3236
Joshua Herrmann <jdvh1292@gmail.com>
2023-12-02 13:43:17 -0500

    Finished D1P2

--- day1/main.go
+++ day1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"time"
@@ -41,8 +42,7 @@ func part1() {
 
 	sum := 0
 
-	regexMatchPattern := `[0-9]`
-	regexMatch := regexp.MustCompile(regexMatchPattern)
+	regexMatch := regexp.MustCompile(`\d`)
 
 	for _, line := range inputLineArray {
 		numbers := regexMatch.FindAllString(line, -1)
@@ -54,49 +54,67 @@ func part1() {
 }
 
 func part2() {
-	inputLineArray := read.ReadStrArrayByLine("./exinput.txt")
-
+	inputLineArray := read.ReadStrArrayByLine("./input.txt")
 	sum := 0
-
 	//leaving for reference, doesn't work because golang regex does not support overlapping matches like 'eightwo'
-	//digitMatchPattern := `([1-9]|one|two|three|four|five|six|seven|eight|nine)`
-
-	digitMatchPattern := `[1-9]`
-	digitMatch := regexp.MustCompile(digitMatchPattern)
-	oneMP := `one`
-	oneM := regexp.MustCompile(oneMP)
-	twoMP := `two`
-	twoM := regexp.MustCompile(twoMP)
-	threeMP := `three`
-	threeM := regexp.MustCompile(threeMP)
-	fourMP := `four`
-	fourM := regexp.MustCompile(fourMP)
-	fiveMP := `five`
-	fiveM := regexp.MustCompile(fiveMP)
-	sixMP := `six`
-	sixM := regexp.MustCompile(sixMP)
-	sevenMP := `seven`
-	sevenM := regexp.MustCompile(sevenMP)
-	eightMP := `eight`
-	eightM := regexp.MustCompile(eightMP)
-	nineMP := `nine`
-	nineM := regexp.MustCompile(nineMP)
+	//digitMatch := regexp.MustCompile(`([1-9]|one|two|three|four|five|six|seven|eight|nine)`)
+	digitMatch := regexp.MustCompile(`\d`)
+	oneM := regexp.MustCompile(`one`)
+	twoM := regexp.MustCompile(`two`)
+	threeM := regexp.MustCompile(`three`)
+	fourM := regexp.MustCompile(`four`)
+	fiveM := regexp.MustCompile(`five`)
+	sixM := regexp.MustCompile(`six`)
+	sevenM := regexp.MustCompile(`seven`)
+	eightM := regexp.MustCompile(`eight`)
+	nineM := regexp.MustCompile(`nine`)
 
 	for _, line := range inputLineArray {
+		limitMatches := map[string][]int{
+			"first": {math.MaxInt, math.MaxInt},
+			"last":  {0, 0},
+		}
 		numbers := digitMatch.FindAllStringIndex(line, -1)
 		ones := oneM.FindAllStringIndex(line, -1)
 		twos := twoM.FindAllStringIndex(line, -1)
 		threes := threeM.FindAllStringIndex(line, -1)
 		fours := fourM.FindAllStringIndex(line, -1)
 		fives := fiveM.FindAllStringIndex(line, -1)
-		sixs := sixM.FindAllStringIndex(line, -1)
+		sixes := sixM.FindAllStringIndex(line, -1)
 		sevens := sevenM.FindAllStringIndex(line, -1)
 		eights := eightM.FindAllStringIndex(line, -1)
 		nines := nineM.FindAllStringIndex(line, -1)
+		matchesLists := [][][]int{
+			numbers, ones, twos, threes, fours, fives, sixes, sevens, eights, nines,
+		}
+		for _, matches := range matchesLists {
+			//fmt.Printf("%v\n", matches)
+			for _, indexPair := range matches {
+				//Find First by checking the starting index of the match
+				// If less, it's earlier in the string
+				if indexPair[0] < limitMatches["first"][0] {
+					// if less, replace the limit matches with this match
+					limitMatches["first"][0] = indexPair[0]
+					limitMatches["first"][1] = indexPair[1]
+				}
+				//Find Last by checking the starting index of the match
+				// If greater, it's later in the string
+				if indexPair[0] > limitMatches["last"][0] {
+					limitMatches["last"][0] = indexPair[0]
+					limitMatches["last"][1] = indexPair[1]
+				}
+			}
+		}
+		firstMatch := line[limitMatches["first"][0]:limitMatches["first"][1]]
+		lastMatch := line[limitMatches["last"][0]:limitMatches["last"][1]]
+		//fmt.Printf("Matched: %s, %s\n", firstMatch, lastMatch)
 
-		first := getNumberString(numbers[0])
-		last := getNumberString(numbers[len(numbers)-1])
-		fmt.Printf("First: %s Last: %s\n", first, last)
+		first := getNumberString(firstMatch)
+		last := getNumberString(lastMatch)
+		if last == "" {
+			last = first
+		}
+		fmt.Printf("%s - %s%s\n", line, first, last)
 		value, _ := strconv.Atoi(fmt.Sprintf("%s%s", first, last))
 		sum += value
 	}
